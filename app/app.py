2db5af33f7086869d7253bfe9c0d5cc19c9d877d
Ernesto Crespo <ernesto@asistensi.com>
2024-07-13 18:32:48 -0400

    Request path and run script

--- app/app.py
+++ app/app.py
@@ -6,3 +6,13 @@ app = FastAPI()
 @app.get("/hello")
 def hello():
     return {"message": "Hello, World!"}
+
+
+@app.get("/hello/{name}")
+def hello_name(name: str):
+    return {"message": f"Hello, {name}!"}
+
+
+@app.get("/hello/{name}/{age}")
+def hello_name_age(name: str, age: int):
+    return {"message": f"Hello, {name}! You are {age} years old."}
