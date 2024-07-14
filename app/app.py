34393c820124c9a999f5436891f96994b3c9fc9d
Ernesto Crespo <ernesto@asistensi.com>
2024-07-13 20:30:25 -0400

    Added request body

--- app/app.py
+++ app/app.py
@@ -1,10 +1,21 @@
 from enum import Enum
-from fastapi import FastAPI, Path
+from pydantic import BaseModel
+
+from fastapi import FastAPI, Path, Body
 
 
 class TipoUsuarioEnum(str, Enum):
     ESTANDARD = "estandard"
     ADMIN = "admin"
+
+
+class Usuario(BaseModel):
+    nombre: str
+    edad: int
+
+
+class Compania(BaseModel):
+    nombre: str
 
 app = FastAPI()
 
@@ -41,3 +52,23 @@ async def get_license_plate(license: str = Path(..., min_length=9, max_length=9)
 @app.get("/license-plates/{license}")
 async def get_license_plate(license: str = Path(..., regex=r"^\w{2}-\d{3}-\w{2}$")):
     return {"license": license}
+
+
+@app.post("/usuarios")
+async def create_user(name: str = Body(...), age: int = Body(...)):
+    return {"name": name, "age": age}
+
+
+@app.post("/usuarios/dos")
+async def crear_usuario(usuario: Usuario):
+    return usuario
+
+
+@app.post("/usuarios/uno")
+async def crear_usuario2(usuario: Usuario, compania: Compania):
+    return {"usuarior": usuario, "compania": compania}
+
+
+@app.post("/usuarios/tres")
+async def create_user(usuario: Usuario, prioridad: int = Body(..., ge=1, le=3)):
+    return {"usuario": usuario, "prioridad": prioridad}
