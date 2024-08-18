b1eed123ffa2665bec15611a89b9c2405c435c04
Ernesto Crespo <ernesto@asistensi.com>
2024-08-18 13:40:31 -0400

    Added websocket server

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
 from app.controllers.index import user
+from app.utils.WebSocket import manager
 
 app = FastAPI(
 title="My API with documentation",
@@ -20,3 +22,22 @@ title="My API with documentation",
 
 
 app.include_router(user)
+
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_message(f"Client {client_id}: {data}", websocket)
+            await manager.broadcast(f"Client {client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client {client_id} disconnected")
+
+
+@app.get("/chat")
+async def get():
+    with open("app/index.html") as f:
+        return HTMLResponse(f.read())
